0aee2ebe402e1375e810b1cfe28aabe983a00077
BaranovVladimir73 <baranov.vladimir1990@gmail.com>
2022-10-16 22:52:16 +0400

    Add Main page controller, writing new article and viewing article

--- src/main/java/ru/gb/hubr/entity/Article.java
+++ src/main/java/ru/gb/hubr/entity/Article.java
@@ -1,6 +1,7 @@
 package ru.gb.hubr.entity;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
 import ru.gb.hubr.entity.common.InfoEntity;
 
 import javax.persistence.*;
@@ -15,6 +16,10 @@ import javax.persistence.*;
 @Table(name = "article")
 public class Article extends InfoEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private AccountUser author;
@@ -24,5 +29,14 @@ public class Article extends InfoEntity {
 
     @Column(name = "content")
     private String content;
+
+    public String getPreview(){
+
+        String[] strings = content.split("</p>");
+
+        String preview = strings[0] + strings[1];
+
+        return preview;
+    }
 
 }
