adeb160b239e98ce0f06617cbdd3e25bb94d586e
Vitaly <woodanon123@gmail.com>
2022-10-24 20:12:07 +0500

    add check mail token

--- src/test/java/ru/gb/hubr/dao/EventUserDaoTest.java
+++ src/test/java/ru/gb/hubr/dao/EventUserDaoTest.java
@@ -3,13 +3,16 @@ package ru.gb.hubr.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import ru.gb.hubr.entity.EventUser;
+import ru.gb.hubr.entity.TypeEvent;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@EnableJpaAuditing
 class EventUserDaoTest {
 
     @Autowired
@@ -18,7 +21,7 @@ class EventUserDaoTest {
     @Test
     public void saveTest() {
 
-        EventUser save = eventUserDao.save(EventUser.builder().lifetimeSeconds(3600).build());
+        EventUser save = eventUserDao.save(EventUser.builder().typeEvent(TypeEvent.CHANGE_EMAIL).build());
         UUID guid_event = save.getGuidEvent();
         save = eventUserDao.save(save);
         assertEquals(save.getDeletedAt(),save.getCreatedAt().plusSeconds(save.getLifetimeSeconds()));
