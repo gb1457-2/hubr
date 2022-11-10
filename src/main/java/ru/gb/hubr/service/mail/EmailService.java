5a0522f1b7dc5f30cc25296ce9bbc08780439273
PSelyutin <pselyutin@inno.tech>
2022-11-10 22:59:18 +0400

    restore_default_email_service

--- src/main/java/ru/gb/hubr/service/mail/EmailService.java
+++ src/main/java/ru/gb/hubr/service/mail/EmailService.java
@@ -1,48 +1,8 @@
 package ru.gb.hubr.service.mail;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import ru.gb.hubr.config.MailProperties;
-
 import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 
-@Service
-@RequiredArgsConstructor
-public class EmailService {
-
-
-    private final MailProperties mailProperties;
-
-    @Autowired
-    private JavaMailSender emailSender;
-
-    @Autowired
-    private SpringTemplateEngine templateEngine;
-
-    public void sendMail(EmailContext email) throws MessagingException, UnsupportedEncodingException {
-
-        MimeMessage message = emailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message,
-                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-                StandardCharsets.UTF_8.name());
-        Context context = new Context();
-        context.setVariables(email.getContext());
-        String emailContent = templateEngine.process(mailProperties.getPathTemplate(), context);
-
-        mimeMessageHelper.setTo(email.getTo());
-        mimeMessageHelper.setSubject(email.getSubject());
-        mimeMessageHelper.setFrom(email.getFrom(), email.getDisplayName());
-        mimeMessageHelper.setText(emailContent, true);
-        emailSender.send(message);
-    }
-
-
+public interface EmailService {
+    void sendMail(EmailContext EmailContext) throws MessagingException, UnsupportedEncodingException;
 }
